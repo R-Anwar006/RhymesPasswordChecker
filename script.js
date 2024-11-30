278a8a9c48b658c8f9c45278c9738617af757b44
R-Anwar006 <rhymeanwar06@gmail.com>
2024-11-29 21:10:58 -0500

    Update script.js
    Added a no password insert response

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function checkpassword() {
+function checkpassword() 
+{
     const password = document.getElementById('password').value;
     const result = document.getElementById('result');
 
@@ -8,23 +9,34 @@ function checkpassword() {
     const numberpass = /\d/.test(password);
     const symbolpass = /[!@#$%^&*(),.?":{}|<>]/.test(password);
     const capitalpass = /[A-Z]/.test(password);
+    const nopass = password.length <= 0;
 
     let strength = 0;
     if (lengthpass) strength++;
     if (numberpass) strength++;
     if (symbolpass) strength++;
     if (capitalpass) strength++;
+    if (nopass) strength--;
 
-    console.log("Strength level: " + strength);  // Debugging line
+    console.log("Strength level: " + strength);  
 
-    if (strength === 4) {
+    if (strength === 4) 
+    {
         result.textContent = "Your password is a strong password!";
         result.style.color = "green";
-    } else if (strength === 3) {
+    } 
+    else if (strength === 3)
+    {
         result.textContent = "Your password is manageable..";
         result.style.color = "orange";
-    } else {
+    } 
+    else if (strength == 0)
+    {
         result.textContent = "Your password is too weak!";
         result.style.color = "red";
     }
+    else if (strength >= -1)
+    {
+        result.textContent = "You didnt insert a password!";
+        result.style.color = "red";
 }
