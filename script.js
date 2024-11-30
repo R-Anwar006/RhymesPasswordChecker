61ff1903018b1d2ba633f958aba1b1d924681d6c
R-Anwar006 <rhymeanwar06@gmail.com>
2024-11-29 21:37:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function checkpassword() {
     const lowercasepass = /[a-z]/.test(password); // to check for lower case letters
     const nopass = password.length <= 0;      // to check if no password is entered
 
-    // initialize strength level at 0
-    let strength = 0;
+    // initialize strength level at 1
+    let strength = 1;
 
     // increase the strength for each criteria met
     if (lengthpass) strength++;
@@ -33,23 +33,23 @@ function checkpassword() {
         result.style.color = "green";
     } 
     else if (strength === 5) {
-        result.textContent = "your password is strong.";
+        result.textContent = "Your password is strong.";
         result.style.color = "green";
     } 
     else if (strength === 4) {
-        result.textContent = "your password is manageable.";
+        result.textContent = "Your password is manageable.";
         result.style.color = "orange";
     } 
     else if (strength === 3) {
-        result.textContent = "your password is alright.";
+        result.textContent = "Your password is alright.";
         result.style.color = "yellow";
     } 
     else if (strength === 2) {
-        result.textContent = "your password is weak!";
+        result.textContent = "Your password is weak!";
         result.style.color = "red";
     } 
     else if (strength === 1) {
-        result.textContent = "your password is very weak!";
+        result.textContent = Yyour password is very weak!";
         result.style.color = "red";
     }
     else if (strength <= 0) {
@@ -59,7 +59,7 @@ function checkpassword() {
 }
 
 function generatePassword() {
-    console.log("generating password...");  // debugging line
+    console.log("Generating password...");  // debugging line
     const length = 14;
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
     let password = '';
@@ -69,6 +69,6 @@ function generatePassword() {
     }
 
     const display = document.getElementById('generated-password');
-    display.textContent = `here is your password: ${password}`;
+    display.textContent = `Here is your password: ${password}`;
     display.style.color = "blue";
 }
